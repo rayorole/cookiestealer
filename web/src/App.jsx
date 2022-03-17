bb8511e7f27c0da9aa230eba018669d4248f129c
Ray Orolé <orole.ray@protonmail.com>
2022-03-17 20:03:28 +0100

    Add UI

---
+++ web/src/App.jsx
@@ -0,0 +1,12 @@
+import logo from "./assets/logo.svg";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hey</h1>
+    </div>
+  );
+}
+
+export default App;
